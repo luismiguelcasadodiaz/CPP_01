2ebdb62011b250a886f5bbfc3462eab277a6ebaf
Luis Miguel Casado Díaz <luismiguelcasadodiaz@gmail.com>
2024-12-28 20:18:01 +0000

    Wrote itoa. I use it to give differente names to Zombies. Add a check for negative number of zombies to create. I N is negative it wil do nothing

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -12,12 +12,45 @@
 
 #include "Zombie.class.hpp"
 
+void	itoa (int N, std::string *resultado)
+{
+	char		num;
+
+	if (N == 0)
+		resultado->insert(0, 1, '0');
+	else if (N > 0)
+	{ 
+		if (N  > 9)
+		{
+			itoa(N / 10, resultado);
+			num = static_cast<char>(48 + N % 10);
+		}
+		num = static_cast<char>(48 + N % 10);
+		resultado->append(1,  num);
+	}
+		
+}
+
 Zombie *zombieHorde (int N, std::string name)
 {
-	Zombie *the_zombie = new Zombie[N];
 	int		idx = 0;
+	std::string	num_txt;
+       
+	itoa(125, &num_txt);
 
-	while (idx < N)
-		the_zombie[idx++].setName(name);
-	return &(the_zombie[0]);
+	std::cout << "tengo que crear " << num_txt << std::endl;
+	if (N > 0)
+	{
+		Zombie *the_zombie = new Zombie[N];
+		while (idx < N)
+		{
+			num_txt.clear();
+			itoa(idx, &num_txt);
+			//std::string myname = name.append(num_txt.size(), num_txt);
+			the_zombie[idx++].setName(name + num_txt);
+		}
+		return &(the_zombie[0]);
+	}
+	else
+		return (NULL);
 }
